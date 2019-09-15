102a81cc73f01538c8003864e4abbefab0ba9cc5
Samir70 <sbetmouni@talktalk.net>
2019-09-15 20:39:41 +0100

    Updates

---
+++ precache-manifest.4036ce9d6068d6c977665b27bf6e3dd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da2207bebaa34cf8a7d34525e5d2664d",
+    "url": "/maths-elo/index.html"
+  },
+  {
+    "revision": "9dfcd49cd909aef12000",
+    "url": "/maths-elo/static/css/main.8660db44.chunk.css"
+  },
+  {
+    "revision": "bf0c9b1f482ddb36edfa",
+    "url": "/maths-elo/static/js/2.7cc10581.chunk.js"
+  },
+  {
+    "revision": "9dfcd49cd909aef12000",
+    "url": "/maths-elo/static/js/main.ac75f8e1.chunk.js"
+  },
+  {
+    "revision": "1c539b3cb7f5b39458a3",
+    "url": "/maths-elo/static/js/runtime~main.865c917a.js"
+  }
+]);
